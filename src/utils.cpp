5a7c885f8211078ad8c4057b3344d584cbb2d285
Karandeep Johar <karandeepjohar@gmail.com>
2016-04-15 01:53:55 -0400

    bug fix

--- src/utils.cpp
+++ src/utils.cpp
@@ -12,7 +12,134 @@
 
 #include "utils.hpp"
 #include "internal_config.hpp"
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <ctime>
 
+template <class T>
+double gen_uniform_normalized_double(T& random)
+{
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+    return dist(random);
+}
+
+template <class T>
+class alias_method
+{
+public:
+    /**
+     * Constructs a new alias_method to sample from a discrete distribution and
+     * hand back outcomes based on the probability distribution.
+     * <p>
+     * Given as input a list of probabilities corresponding to outcomes 0, 1,
+     * ..., n - 1, along with the random number generator that should be used
+     * as the underlying generator, this constructor creates the probability 
+     * and alias tables needed to efficiently sample from this distribution.
+     *
+     * @param probabilities The list of probabilities.
+     * @param random The random number generator
+     */
+    alias_method(const std::vector<double>& probability, T& random)
+        : probability_(probability), random_(random)
+    {
+    // Allocate space for the alias table.
+        alias_.resize(probability_.size());
+
+        // Compute the average probability and cache it for later use.
+        const double average = 1.0 / probability_.size();
+
+        // two stacks to act as worklists as we populate the tables
+        std::vector<int> small, large;
+
+        // Populate the stacks with the input probabilities.
+        for(size_t i=0; i<probability_.size(); ++i)
+        {
+            // If the probability is below the average probability, then we add
+            // it to the small list; otherwise we add it to the large list.
+            if (probability_[i] >= average)
+                large.push_back(i);
+            else
+                small.push_back(i);
+        }
+
+        // As a note: in the mathematical specification of the algorithm, we
+        // will always exhaust the small list before the big list.  However,
+        // due to floating point inaccuracies, this is not necessarily true.
+        // Consequently, this inner loop (which tries to pair small and large
+        // elements) will have to check that both lists aren't empty.
+        while(!small.empty() && !large.empty()) 
+        {
+            // Get the index of the small and the large probabilities.
+            int less = small.back(); small.pop_back();
+            int more = large.back(); large.pop_back();
+
+            alias_[less] = more;
+
+            // Decrease the probability of the larger one by the appropriate
+            // amount.
+            probability_[more] = (probability_[more] + probability_[less]) - average;
+
+            // If the new probability is less than the average, add it into the
+            // small list; otherwise add it to the large list.
+            if (probability_[more] >= average)
+                large.push_back(more);
+            else
+                small.push_back(more);
+        }
+
+        // At this point, everything is in one list, which means that the
+        // remaining probabilities should all be 1/n.  Based on this, set them
+        // appropriately.  Due to numerical issues, we can't be sure which
+        // stack will hold the entries, so we empty both.
+        while(!small.empty())
+        {
+            probability_[small.back()] = average;
+            small.pop_back();
+        }
+        while(!large.empty())
+        {
+            probability_[large.back()] = average;
+            large.pop_back();
+        }
+        
+        // These probabilities have not yet been scaled up to be such that
+        // 1/n is given weight 1.0.  We do this here instead.
+        int n = static_cast<int>(probability_.size());
+        std::transform(probability_.cbegin(), probability_.cend(), probability_.begin(), 
+            [n](double p){ return p * n; });
+    }
+
+    /**
+     * Samples a value from the underlying distribution.
+     *
+     * @return A random value sampled from the underlying distribution.
+     */
+    int next()
+    {
+        // Generate a fair die roll to determine which column to inspect.
+        int column = random_() % probability_.size();
+        // Generate a biased coin toss to determine which option to pick.
+        bool coinToss = gen_uniform_normalized_double(random_) < probability_[column];
+
+        // Based on the outcome, return either the column or its alias.
+        return coinToss? column : alias_[column];
+    }
+
+private:
+  // The probability and alias tables.
+    std::vector<int> alias_;
+    std::vector<double> probability_;
+  // The random number generator used to sample from the distribution.
+    T& random_;
+};
+
+template <class T>
+alias_method<T> make_alias_method(const std::vector<double>& probabilities, T& random)
+{
+    return alias_method<T>(probabilities, random);
+}
 
 unsigned long long calculateAvailableRAM( const unsigned long long totalRAM, const double memUse ) {
 	return static_cast<unsigned long long>( static_cast<double>(totalRAM)*memUse );
@@ -97,12 +224,58 @@ void ShatterSquare( std::vector<Square>& square, const double a, const double b,
 
 }
 
+std::pair<unsigned long long, unsigned long long> get_Edge_indices( unsigned long long offX, unsigned long long rngX,unsigned long long offY, unsigned long long rngY, const double a, const double b,const double c,std::uniform_int_distribution<>& dis, std::mt19937_64& gen ) {
+	// std::vector<double> probabilities;
+ //    probabilities.push_back(a);
+ //    probabilities.push_back(b);
+ //    probabilities.push_back(c);
+ //    probabilities.push_back(1-(a+b+c));
+ //    std::mt19937_64 rnd;
+ //    rnd.seed(clock());
+ //    auto am = make_alias_method(probabilities, rnd);
+
+    double sumA = a;
+    double sumAB = a+b;
+    double sumAC = a+c;
+    double sumABC = a+b+c;
+
+      // rngX = Nodes;  rngY = Nodes;  offX = startIdx_ull;  offY = 0;
+      // Depth = 0;
+      // recurse the matrix
+      while (rngX > 1 || rngY > 1) {
+        const double RndProb = (static_cast<double>(dis(gen))/dis.max());
+        if (rngX>1 && rngY>1) {
+          if (RndProb < sumA) { rngX/=2; rngY/=2; }
+          else if (RndProb < sumAB) { offX+=rngX/2;  rngX-=rngX/2;  rngY/=2; }
+          else if (RndProb < sumABC) { offY+=rngY/2;  rngX/=2;  rngY-=rngY/2; }
+          else { offX+=rngX/2;  offY+=rngY/2;  rngX-=rngX/2;  rngY-=rngY/2; }
+        } else
+        if (rngX>1) { // row vector
+          if (RndProb < sumAC) { rngX/=2; rngY/=2; }
+          else { offX+=rngX/2;  rngX-=rngX/2;  rngY/=2; }
+        } else
+        if (rngY>1) { // column vector
+          if (RndProb < sumAB) { rngX/=2; rngY/=2; }
+          else { offY+=rngY/2;  rngX/=2;  rngY-=rngY/2; }
+        } else{
+    	        throw std::invalid_argument( "received negative value" );
+
+        }
+      }
+
+     	return 	std::make_pair (offX, offY);
+    
+}
+
+
 unsigned long long genEdgeIndex_FP( unsigned long long startIdx_ull, unsigned long long endIdx_ull, const double a, const double b_or_c, std::uniform_int_distribution<>& dis, std::mt19937_64& gen ) {
 
 	double noise_a = 0, noise_b_or_c = 0, cutLine;
 	double cutIndex;
 	auto startIdx = static_cast<double>(startIdx_ull);
 	auto endIdx = static_cast<double>(endIdx_ull);
+
+
 	while( (endIdx - startIdx) >= 1.0 ) {
 	#ifdef ADD_NOISE_TO_RMAT_PARAMETERS_AT_EACH_LEVEL
 			noise_a = static_cast<double>(dis(gen)-(dis.max()/2.0))/(dis.max()*500.0);	// Much smaller noise. Maybe can be improved.
@@ -152,8 +325,18 @@ void generate_edges( Square& squ,
 	auto createNewEdges = duplicate_indices.empty();
 	unsigned long long nEdgesToGen = createNewEdges ? squ.getnEdges() : duplicate_indices.size();
 	for( unsigned long long edgeIdx = 0; edgeIdx < nEdgesToGen; ) {
-		unsigned long long h_idx = genEdgeIndex_FP(squ.get_X_start(), squ.get_X_end(), RMAT_a, RMAT_c, std::ref(dis), std::ref(gen));
-		unsigned long long v_idx = genEdgeIndex_FP(squ.get_Y_start(), squ.get_Y_end(), RMAT_a, RMAT_b, std::ref(dis), std::ref(gen));
+		unsigned long long h_idx, v_idx;
+		std::pair <long long int,long long int> e;
+		unsigned long long offX, offY, rngX, rngY;
+		offX = squ.get_X_start();
+		rngX = squ.get_X_end()-offX;
+		offY = squ.get_Y_start();
+		rngY = squ.get_Y_end()-offY;
+		e = get_Edge_indices(offX, rngX, offY, rngY,RMAT_a,RMAT_b, RMAT_c,  std::ref(dis), std::ref(gen) );
+		h_idx = e.first;
+		v_idx = e.second;
+		// unsigned long long h_idx = genEdgeIndex_FP(squ.get_X_start(), squ.get_X_end(), RMAT_a, RMAT_c, std::ref(dis), std::ref(gen));
+		// unsigned long long v_idx = genEdgeIndex_FP(squ.get_Y_start(), squ.get_Y_end(), RMAT_a, RMAT_b, std::ref(dis), std::ref(gen));
 		if( (!applyCondition && h_idx > v_idx) || (!allowEdgeToSelf && h_idx == v_idx ) ) // Short-circuit if it doesn't pass the test.
 			continue;
 		if( createNewEdges )	// Create new edges.
